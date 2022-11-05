ff8ab921d747bd5f7f325d18268dc09ae55e26b2
paula bonett <agudeloalejandra545@gmail.com>
2022-11-04 20:19:15 -0500

    gggg

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
@@ -7,6 +7,7 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 })
 export class GifsService {
   private apiKey:string='QZZhAN2RbSRfUWZhyrVp81FoezaxaRMm'
+  private servicioUrl:string='https://api.giphy.com/v1/gifs'
   private _historial:string[]=[]
   
   public resultado:Gif[]=[]
@@ -32,7 +33,12 @@ export class GifsService {
 
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=QZZhAN2RbSRfUWZhyrVp81FoezaxaRMm&q=${query}&limit=10`)
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+          .set('limit', '10')
+          .set('q', query)
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params})
     
       .subscribe((resp)=>{
         console.log(resp.data);
